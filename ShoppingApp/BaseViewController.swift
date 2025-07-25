1d02aa113df49594b7186bd504c5272cd6d17ee3
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-25 16:30:23 +0900

    [Feat] BaseViewController 구현

---
+++ ShoppingApp/BaseViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BaseViewController.swift
+//  ShoppingApp
+//
+//  Created by Lee on 7/25/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .bg
+        
+        configureUI()
+        configureViewHierarchy()
+        configureConstraints()
+
+
+
+    }
+
+    func configureUI() {
+
+    }
+
+    func configureConstraints() {
+
+    }
+
+    func configureViewHierarchy() {
+
+    }
+}
