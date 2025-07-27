d835a7cbe4b7d325cd0b99cd9e9701a8a9920ea0
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-27 20:01:41 +0900

    [Fix] CustomButton border color 동적 대응

--- ShoppingApp/Common/CustomButton.swift
+++ ShoppingApp/Common/CustomButton.swift
@@ -13,12 +13,13 @@ class CustomButton: UIButton {
         super.init(frame: frame)
     }
 
-    init(title: String) {
+    init(title: String, tag: Int) {
         super.init(frame: .zero)
         setTitle(title, for: .normal)
         setTitleColor(.main, for: .normal)
         setTitle(title, for: .selected)
         setTitleColor(.main, for: .selected)
+        self.tag = tag
         configureUI()
     }
 
@@ -29,9 +30,19 @@ class CustomButton: UIButton {
     private func configureUI() {
         titleLabel?.font = UIFont.customFont(.title)
         backgroundColor = .clear
-        layer.borderColor = UIColor.main.cgColor
         layer.borderWidth = 1
         layer.cornerRadius = 8
         clipsToBounds = true
+    }
+
+    private func setupBolderColor() {
+        layer.borderColor = UIColor.main.cgColor
+    }
+
+    // CGColor는 다크모드 자동 대응이 안됨
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        layer.borderColor = UIColor.main.cgColor
     }
 }
