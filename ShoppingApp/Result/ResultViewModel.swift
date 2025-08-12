56351fb77181b93d2fb90bd8559b9d3c662fde8c
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-12 22:12:02 +0900

    [Feat] ResultViewModel 구현

---
+++ ShoppingApp/Result/ResultViewModel.swift
@@ -0,0 +1,117 @@
+//
+//  ResultViewModel.swift
+//  ShoppingApp
+//
+//  Created by Lee on 8/12/25.
+//
+
+import Foundation
+
+/*
+ 데이터 배열로 가공해서 넘겨줄게
+ */
+
+class ResultViewModel {
+
+    let networkManager = NetworkManager.shared
+
+    var currentCategory: Observable<SortingType> = Observable(value: .accuracy)
+
+    var currentItemData: Observable<ResultData> = Observable(value: .init(total: 0, items: []))
+
+    var errorMessage: Observable<String> = Observable(value: "")
+
+    var suggestItemData: Observable<ResultData> = Observable(value: .init(total: 0, items: []))
+
+    var viewDidLoadTrigger: Observable<Void> = Observable(value: ())
+
+    var start = 1
+
+    // init 시점에 주입받는 데이터
+    var keyword: String
+    init(_ keyword: String) {
+        self.keyword = keyword
+        currentCategory.bind { sortType in
+            self.fetchData(sortingType: sortType)
+        }
+
+        viewDidLoadTrigger.bind { _ in
+            self.fetchSuggestData(sortingType: .accuracy)
+        }
+    }
+
+    private func fetchData(sortingType: SortingType) {
+
+        let queries = networkManager.makeNaverSearchQueries(keyword: keyword, display: QueryData.displayNum, start: start, sortingType: sortingType.rawValue)
+
+        guard let url = networkManager.getURL(scheme: APIData.scheme.rawValue, host: APIData.host.rawValue, path: APIData.path.rawValue, queries: queries) else {
+            print(NetworkError.invalidURL.errorMessage)
+            return
+        }
+
+        guard let headers = APIData.headers else { return }
+
+        networkManager.fetchData(headers: headers, url: url) { (response: Result<ResultData, Error>) in
+            switch response {
+            case .success(let data):
+                self.currentItemData.value = data
+                // self.currentItemData.value.append(contentsOf: data.items)
+
+            case .failure(let error):
+                if let searchError = error as? SearchError {
+                    switch searchError {
+                    case .serverError:
+                        if let code = searchError.serverErrorLog {
+                            print(code.rawValue)
+                            print(code.description)
+                        }
+                        self.errorMessage.value = searchError.serverErrorLog?.userMessage ?? ""
+                    default:
+                        print(error.localizedDescription)
+                    }
+                } else if let networkError = error as? NetworkError {
+                    switch networkError {
+                    default:
+                        self.errorMessage.value = networkError.userMessage
+                    }
+                } else {
+                    print(NetworkError.unKnownError.errorMessage)
+                }
+            }
+        }
+    }
+
+    private func fetchSuggestData(sortingType: SortingType, display: Int = QueryData.displayNum) {
+
+        let keyword = "공룡"
+
+        let queries = networkManager.makeNaverSearchQueries(keyword: keyword, display: QueryData.displayNum, start: start, sortingType: sortingType.rawValue)
+
+        guard let url = networkManager.getURL(scheme: APIData.scheme.rawValue, host: APIData.host.rawValue, path: APIData.path.rawValue, queries: queries) else { return }
+
+        guard let headers = APIData.headers else { return }
+
+        networkManager.fetchData(headers: headers, url: url) { (response: Result<ResultData, Error>) in
+            switch response {
+            case .success(let data):
+                self.suggestItemData.value = data
+            case .failure(let error):
+                if let searchError = error as? SearchError {
+                    switch searchError {
+                    case .serverError:
+                        if let code = searchError.serverErrorLog {
+                            print(code.rawValue)
+                            print(code.description)
+                        }
+
+                        self.errorMessage.value = searchError.serverErrorLog?.userMessage ?? ""
+                    default:
+                        print(error.localizedDescription)
+                        return
+                    }
+                }
+            }
+        }
+    }
+
+}
