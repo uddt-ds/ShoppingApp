6f4a9a9aa0ca6a1574a90ac5aabf180d15ff1d5e
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-13 21:28:58 +0900

    [Refactor] SearchRouter 구현에 따른 Networkmanager 코드 변경

--- ShoppingApp/Common/NetworkManager.swift
+++ ShoppingApp/Common/NetworkManager.swift
@@ -13,61 +13,41 @@ class NetworkManager {
 
     private init() { }
 
-    // 파라미터를 모델로 만들고, 제네릭 타입이나 프로토콜 활용
-    // 라우터 패턴 (나중에 리캡 다 끝나고 공부)
-
-    // MARK: URL 정의하는 메서드
-    func getURL(scheme: String, host: String, path: String, queries: [URLQueryItem]) -> URL? {
-        var components = URLComponents()
-        components.scheme = scheme
-        components.host = host
-        components.path = path
-        components.queryItems = queries
-        return components.url
-    }
-
-    // MARK: 데이터 불러오는 제네릭 메서드
-    func fetchData<T: Decodable>(headers: HTTPHeaders, url: URL, completion: @escaping (Result<T, Error>) -> Void) {
-        AF.request(url, headers: headers)
-            .responseDecodable(of: T.self) { responseData in
-                guard let statusCode = responseData.response?.statusCode else {
-                    completion(.failure(NetworkError.invalidURL))
-                    return
-                }
-                switch statusCode {
-                case 200..<300:
-                    switch responseData.result {
-                    case .success(let data):
-                        completion(.success(data))
-                    case .failure(let error):
-                        print(error.localizedDescription)
-                        completion(.failure(NetworkError.failDecoding))
-                    }
-                case 400...500:
-                    if let data = responseData.data {
-                        do {
-                            let errorData = try JSONDecoder().decode(ServerError.self, from: data)
-                            completion(.failure(SearchError.serverError(code: errorData.errorCode)))
-                        } catch {
-                            completion(.failure(NetworkError.noData))
-                        }
-                    }
-                default:
-                    completion(.failure(NetworkError.unKnownError))
-                }
-
+    func callRequest<T: Decodable>(api: SearchRouter,
+                                   type: T.Type,
+                                   completion: @escaping (Result<T, Error>) -> Void) {
+        guard let url = api.endPoint else { return }
+        AF.request(url,
+                   method: api.method,
+                   parameters: api.parameter,
+                   encoding: URLEncoding(destination: .queryString),
+                   headers: api.headers).responseDecodable(of: T.self) { responseData in
+            guard let statusCode = responseData.response?.statusCode else {
+                completion(.failure(NetworkError.invalidURL))
+                return
             }
-    }
+            switch statusCode {
+            case 200..<300 :
+                switch responseData.result {
+                case .success(let data):
+                    completion(.success(data))
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    completion(.failure(NetworkError.failDecoding))
+                }
 
-    // 얘는 종속된 메서드(네이버에만 쓸 수 있는 상태)
-    func makeNaverSearchQueries(keyword: String, display: Int, start: Int, sortingType: SortingType.RawValue) -> [URLQueryItem] {
-        let queries = [
-            URLQueryItem(name: "query", value: keyword),
-            URLQueryItem(name: "display", value: "\(display)"),
-            URLQueryItem(name: "start", value: "\(start)"),
-            URLQueryItem(name: "sort", value: sortingType)
-        ]
-
-        return queries
+            case 400...500:
+                if let data = responseData.data {
+                    do {
+                        let errorData = try JSONDecoder().decode(ServerError.self, from: data)
+                        completion(.failure(SearchError.serverError(code: errorData.errorCode)))
+                    } catch {
+                        completion(.failure(NetworkError.noData))
+                    }
+                }
+            default:
+                completion(.failure(NetworkError.unKnownError))
+            }
+        }
     }
 }
